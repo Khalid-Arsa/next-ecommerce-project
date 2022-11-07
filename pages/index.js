c2f842a5adcedfb28931573fd890c82543288aab
Khalid Sanggoyod <khalid.sanggoyod@fligno.com>
2022-11-07 14:00:40 +0800

    React not found issuse | fixed

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,23 @@
 import React from 'react'
+import { Product, FooterBanner, HeroBanner } from '../components'
 
 const Home = () => {
   return (
     <>
-      HeroBanner
+      <HeroBanner />
 
-      <div>
+      <div className='products-heading'>
         <h2>Best Selling Products</h2>
         <p>Speakers of many variation</p>
       </div>
 
-      <div>
+      <div className='products-container'>
         {["Products 1", "Products 2"].map((prod) => (
           prod
         ))}
       </div>
 
-      Footer
+      <FooterBanner />
     </>
   )
 }
